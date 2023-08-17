a8a845fe3e6b0487fe33759ee38eca9128cc75c7
Alanjcabrera <cabreraalan986@gmail.com>
2023-08-17 18:47:53 -0300

    carrito

--- JS/index.js
+++ JS/index.js
@@ -1,43 +1,55 @@
-let nombre = prompt("Ingrese su nombre por favor.");
+let opcion =1;
 
-let capital =parseInt (prompt("Hola " + nombre + " ¿Cual es el monto que quiere solicitar?"));
+const productos =[
+    {nombre:" Sillon gamer TX ", color:" Rojo ", precio:30000},
+    {nombre:" Sillon gamer SX ", color:" Azul ", precio:42000},
+    {nombre:" Sillon gamer FX ", color:" Negra ", precio:65500},
+    {nombre:" Sillon gamer HX ", color:" Blanca ", precio:70800}
+]
+alert("Bienvenido a nuestra tienda de sillas para pc. A continuacion le mostramos los productos disponibles para ofrecerles.");
 
-let cuota = prompt("Cuotas disponibles: 3, 6, 12 y 24 ¿En cuantas cuotas quiere solicitarlo? ");
-switch(cuota){
-    case"3":
-    function suma(capital){
-        let interes1= 0.25;
-        let resultado = capital * interes1;
-        let resultadoEn3 = resultado + capital;
-        alert("El interes en 3 cuotas es de $" + resultadoEn3);
+productos.forEach(sillon => {
+    const mostrarProductos=  `nombre: ${sillon.nombre} color: ${sillon.color} precio: ${sillon.precio}`;
+    alert("Opcion " + opcion + " " + mostrarProductos);
+    opcion++
+});
+
+let opcionElegida;
+let opcionValida = false;
+
+do {
+  opcionElegida = prompt("Si le gustó y quiere comprar uno de nuestros productos, ingrese la opción correspondiente.");
+  switch (opcionElegida) {
+    case "1":
+      alert("Excelente, usted ingresó la opción número 1: " + productos[0].nombre);
+      opcionValida = true;  
+      break;
+    case "2":
+      alert("Excelente, usted ingresó la opción número 2: " + productos[1].nombre);
+      opcionValida = true; 
+      break;
+    case "3":
+      alert("Excelente, usted ingresó la opción número 3: " + productos[2].nombre);
+      opcionValida = true; 
+      break;
+    case "4":
+      alert("Excelente, usted ingresó la opción número 4: " + productos[3].nombre);
+      opcionValida = true; 
+      break;
+    default:
+      alert("La opción ingresada no es correcta, vuelva a intentar.");
+      break;
+  }
+} while (!opcionValida);
+
+let a =prompt("El producto se agrego exitosamente al carrito. ¿Desea continuar con la compra? SI para continuar, NO para cerrar.").toLowerCase();
+
+if(a==si){
+let b=prompt("desea abonar en un pago o en cuotas? ingrese 1 para abonar en un pago. Ingrese 2 para abonar en cuotas.");
+    if (b==1){
     }
-    suma(capital);
-    break;
-    case"6":
-    function suma2(capital){
-        let interes2 =0.50;
-        let resultado2 = capital * interes2;
-        let resultadoEn6 = resultado2 + capital;
-        alert("El interes en 6 cuotas es de $" + resultadoEn6);
-    }
-    suma2(capital);
-    break;
-    case"12":
-    function suma3(capital){
-        interes3= 0.75;
-        let resultado3 = capital * interes3;
-        let resultadoEn12 = resultado3 + capital;
-        alert("El interes en 12 cuotas es de $" + resultadoEn12);
-    }
-    suma3(capital);
-    break;
-    case"24":
-    function suma4(capital){
-        interes4= 1.0;
-        let resultado4 = capital * interes4;
-        let resultadoEn24 = resultado4 + capital;
-        alert("El interes en 24 cuotas es de $" + resultadoEn24);
-    }
-    suma4(capital);
-    break;
+}else{
+    alert("Muchas gracias!")
 }
+
+
