09d23d87b2786b24ce4571f557c0213287dbd210
Alanjcabrera <cabreraalan986@gmail.com>
2023-09-10 21:12:37 -0300

    finalizando el proyecto

--- JS/productos.js
+++ JS/productos.js
@@ -3,28 +3,28 @@ const productosCarrito = [
     precio:82990,
     id: 1,
     title: "Sillon Gamer Negro/Blanco",
-    img: "/IMG/sillauno.jpg",  
+    img: "/IMG/sillauno.png",  
     cantidad: 1,
 },
     {
     precio:80990,
     id: 2,
     title: "Sillon Gamer Negro",
-    img: "/IMG/sillados.jpg", 
+    img: "/IMG/sillados.png", 
     cantidad: 1,  
 },
     {
     precio:76990,
     id: 3,
     title: "Sillon Gamer Negro/Azul",
-    img: "/IMG/sillatres.jpg",
+    img: "/IMG/sillatres.png",
     cantidad: 1,  
 },
     {
     precio:75990,
     id: 4,
     title: "Sillon Gamer Negro/Rojo",
-    img: "/IMG/sillacuatro.jpg", 
+    img: "/IMG/sillacuatro.png", 
     cantidad: 1,  
 }
 ];
